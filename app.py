18fb5b0b48d1d30a8f9dc137bbf5b5ce852b12c6
Ayush <me21b1076@iiitdm.ac.in>
2023-08-22 02:30:04 +0530

    Showing the recommendation

--- app.py
+++ app.py
@@ -2,9 +2,22 @@ import streamlit as st
 from pickle import load
 from pandas import DataFrame
 
-# Loading all the data through pkl files
+# Loading all the movies through pkl files
 movie_dict = load(open('movie_data_dict.pkl','rb'))
+Similarity_Score = load(open('SimScr.pkl','rb'))
 movies = DataFrame(movie_dict)
+
+def recommend(movie):
+    mov_Idx = movies[movies['title']==movie].index[0]
+    distances = Similarity_Score[mov_Idx]
+    movieList = sorted(list(enumerate(distances)), reverse=True, key=lambda x :x[1])[1:8]
+    recommended = []
+
+    for mv in movieList:
+        recommended.append(movies.title.iloc[mv[0]])
+
+    return recommended
+
 
 st.title("Movie Recommender")
 
@@ -15,4 +28,6 @@ selected_movie = st.selectbox(
 
 # placing a button
 if st.button('Recommend'):
-    st.write(selected_movie)
+    recommended_movies = recommend(selected_movie)
+    for mov in recommended_movies:
+        st.write(mov)
