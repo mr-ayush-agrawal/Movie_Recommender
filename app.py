5f605c8dd0181daf51380fc9daf54b2c03ae649f
Ayush <me21b1076@iiitdm.ac.in>
2023-08-22 13:37:10 +0530

    Through API File

--- app.py
+++ app.py
@@ -8,10 +8,12 @@ movie_dict = load(open('movie_data_dict.pkl','rb'))
 Similarity_Score = load(open('SimScr.pkl','rb'))
 movies = DataFrame(movie_dict)
 
+with open('API_Key.txt', 'r') as file:
+    API_KEY = file.readline()
+
 def fetch_poster(movie_id):
-    URL = f'https://api.themoviedb.org/3/movie/{movie_id}?api_key=8265bd1679663a7ea12ac168da84d2e8&&language=en-US'
+    URL = f'https://api.themoviedb.org/3/movie/{movie_id}?api_key={API_KEY}&&language=en-US'
     response = req.get(URL)
-    # print(response.json()['poster_path'])
     return "https://image.tmdb.org/t/p/original" + response.json()['poster_path']
 
 
@@ -19,7 +21,6 @@ def recommend(movie):
     mov_Idx = movies[movies['title']==movie].index[0]
     distances = Similarity_Score[mov_Idx]
     movieList = sorted(list(enumerate(distances)), reverse=True, key=lambda x :x[1])[1:8]
-    # print("MOvie lise ois ------> ",movieList)
     recommended = []
     poster = []
 
@@ -44,8 +45,6 @@ if st.button('Recommend'):
     col = st.columns(7)
     for i in range(7) :
         with col[i]:
-            st.header(names[i])
+            st.text(names[i])
             st.image(poster[i])
-    
-    # for mov in names:
-    #     st.write(mov)
+            
